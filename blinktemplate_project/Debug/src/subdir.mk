990fdab0d1e6b3ef5092eb59d89c94248af5e67a
cwalker <ce.walker@live.com>
2017-11-26 17:46:18 -0500

    Initial commit

---
+++ blinktemplate_project/Debug/src/subdir.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/_write.c \
+../src/main.c \
+../src/stm32f4xx_hal_msp.c \
+../src/stm32f4xx_it.c \
+../src/system_stm32f4xx.c 
+
+OBJS += \
+./src/_write.o \
+./src/main.o \
+./src/stm32f4xx_hal_msp.o \
+./src/stm32f4xx_it.o \
+./src/system_stm32f4xx.o 
+
+C_DEPS += \
+./src/_write.d \
+./src/main.d \
+./src/stm32f4xx_hal_msp.d \
+./src/stm32f4xx_it.d \
+./src/system_stm32f4xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU ARM Cross C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -fno-move-loop-invariants -Wall -Wextra  -g3 -DDEBUG -DSTM32F401xE -DTRACE -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4xx" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ blinktemplate_project/Debug/system/src/cmsis/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_UPPER_SRCS += \
+../system/src/cmsis/startup_stm32f401xe.S 
+
+OBJS += \
+./system/src/cmsis/startup_stm32f401xe.o 
+
+S_UPPER_DEPS += \
+./system/src/cmsis/startup_stm32f401xe.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+system/src/cmsis/%.o: ../system/src/cmsis/%.S
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU ARM Cross Assembler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -fno-move-loop-invariants -Wall -Wextra  -g3 -x assembler-with-cpp -DDEBUG -DTRACE -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4xx" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
